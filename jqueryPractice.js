457aab39663ac9a65c96d86d377a4eaf15093b90
jade0304 <stefanie.dn88@gmail.com>
2021-10-04 13:54:08 -0500

    jquery-practice

---
+++ jqueryPractice.js
@@ -0,0 +1,6 @@
+// window.addEventListener('DOMContentLoaded', (event) => {
+//     console.log('Let’s get ready to party with jQuery!');
+
+//     $('article').addClass('img', '.image-center')
+
+// });
